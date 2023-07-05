57e3196c438e36a724f3124c0f3e3abcc61e3750
Jessie Huzzard <jessiehuzzard@jessies-air.myfiosgateway.com>
2023-07-05 12:31:24 -0400

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+    before_action :set_post, only: [:update, :destroy]
+
+    def create
+      @post = Post.new(post_params)
+  
+      if @post.save
+        render json: @post, status: :created
+      else
+        render json: @post.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def index
+      @posts = Post.all
+      render json: @posts
+    end
+  
+    def update
+      if @post.update(post_params)
+        render json: @post
+      else
+        render json: @post.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @post.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_post
+      @post = Post.find(params[:id])
+    end
+  
+    def post_params
+      params.require(:post).permit(:content, :user_id)
+    end
+end
